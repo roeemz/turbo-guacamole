2c7401a630dfe24b91587f7ecc4b63675802c6a1
Roee Mazor <roee.mazor@appsflyer.com>
2022-09-17 00:00:54 +0200

    Added a linewidths line example

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from io import BytesIO
 from flask import Flask
 from matplotlib.figure import Figure
 
+import numpy as np
+from matplotlib.collections import LineCollection
+
 app = Flask(__name__)
 
 
@@ -19,3 +22,25 @@ def hello():
     # Embed the result in the html output.
     data = base64.b64encode(buf.getbuffer()).decode("ascii")
     return f"<img src='data:image/png;base64,{data}'/>"
+
+@app.route("/var-width-line")
+def hello2():
+    x = np.linspace(0, 4 * np.pi, 10000)
+    y = np.cos(x)
+    lwidths = 1 + x[:-1]
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    lc = LineCollection(segments, linewidths=lwidths, color='blue')
+    fig = Figure()
+    a = fig.subplots()
+    a.add_collection(lc)
+    a.set_xlim(0, 4 * np.pi)
+    a.set_ylim(-1.1, 1.1)
+
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return f"<img src='data:image/png;base64,{data}'/>"
+
