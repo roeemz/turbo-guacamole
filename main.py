bed42b6130a014a6143289adafedd220f5496a42
Roee Mazor <roee.mazor@appsflyer.com>
2022-09-17 00:10:59 +0200

    Added a multicolor example

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from matplotlib.figure import Figure
 import numpy as np
 from matplotlib.collections import LineCollection
 
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap, BoundaryNorm
+
 app = Flask(__name__)
 
 
@@ -36,6 +39,51 @@ def hello2():
     a.add_collection(lc)
     a.set_xlim(0, 4 * np.pi)
     a.set_ylim(-1.1, 1.1)
+
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return f"<img src='data:image/png;base64,{data}'/>"
+
+
+@app.route("/multicolor")
+def hello3():
+    x = np.linspace(0, 3 * np.pi, 500)
+    y = np.sin(x)
+    dydx = np.cos(0.5 * (x[:-1] + x[1:]))  # first derivative
+
+    # Create a set of line segments so that we can color them individually
+    # This creates the points as a N x 1 x 2 array so that we can stack points
+    # together easily to get the segments. The segments array for line collection
+    # needs to be (numlines) x (points per line) x 2 (for x and y)
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+    fig = Figure()
+    axs = fig.subplots(2, 1, sharex=True, sharey=True)
+
+    # Create a continuous norm to map from data points to colors
+    norm = plt.Normalize(dydx.min(), dydx.max())
+    lc = LineCollection(segments, cmap='viridis', norm=norm)
+    # Set the values used for colormapping
+    lc.set_array(dydx)
+    lc.set_linewidth(2)
+    line = axs[0].add_collection(lc)
+    fig.colorbar(line, ax=axs[0])
+
+    # Use a boundary norm instead
+    cmap = ListedColormap(['r', 'g', 'b'])
+    norm = BoundaryNorm([-1, -0.5, 0.5, 1], cmap.N)
+    lc = LineCollection(segments, cmap=cmap, norm=norm)
+    lc.set_array(dydx)
+    lc.set_linewidth(2)
+    line = axs[1].add_collection(lc)
+    fig.colorbar(line, ax=axs[1])
+
+    axs[0].set_xlim(x.min(), x.max())
+    axs[0].set_ylim(-1.1, 1.1)
 
     # Save it to a temporary buffer.
     buf = BytesIO()
